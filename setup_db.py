c2bf6ec6c073d04a72cc1916e9f445b61fd669ba
Zoey VanderMeer <owenvandermeer@gmail.com>
2024-07-22 18:49:08 -0400

    Add sample data generation to database setup

--- setup_db.py
+++ setup_db.py
@@ -41,15 +41,32 @@ def setup():
                         id INTEGER PRIMARY KEY,
                         Group_ID int NOT NULL,
                         User_ID BIGINT NOT NULL,
+                        Personalized_Message CHAR(255),
                         FOREIGN KEY (Group_ID) REFERENCES GROUPS(id),
                         FOREIGN KEY (USER_ID) REFERENCES USERS(id)
                 );  """
 
     db_cursor.execute(member_table)
 
-    print("Database has been configured.")
+    print("Database has been configured")
+
+    db_connection.close()
+
+def generateSampleData():
+    db_connection = sqlite3.connect('befake.sqlite3')
+
+    db_cursor = db_connection.cursor()
+
+    db_cursor.execute('''INSERT INTO USERS (id, User_ID, Guild, Personal_Channel, Username, Display_Name, Real_Name) VALUES (1, 309771442763857931, 1261466340003287134, 1265050555806646386, "cosmosstarlightt", "CosmosStarlightt", "Zoey")''')
+    db_cursor.execute('''INSERT INTO GROUPS (id, Name, Guild, Shared_Channel, Notification_Configuration, Notification_Message, Notification_Time, Notification_Triggered) VALUES (1, "Example Group", 1261466340003287134, 1265051953063530609, "", "Time to befake!", 1721682324, 0)''')
+    db_cursor.execute('''INSERT INTO MEMBERS (id, Group_ID, User_ID, Personalized_Message) VALUES (1, 1, 1, "Time to befake!")''')
+
+    db_connection.commit()
+
+    print("Sample data has been generated")
 
     db_connection.close()
 
 if __name__ == "__main__":
     setup()
+    generateSampleData()
