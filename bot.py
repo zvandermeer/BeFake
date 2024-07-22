fcb7e029a6d720c8ba89bb30bc5f8354a57e2934
Zoey VanderMeer <owenvandermeer@gmail.com>
2024-07-22 15:34:19 -0400

    Update registration to use DB instead of JSON

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import sqlite3
 import discord
 import os
 
@@ -9,6 +10,10 @@ intents.message_content = True
 intents.members = True
 
 client = discord.Client(intents=intents)
+
+db_connection = sqlite3.connect('befake.sqlite3')
+
+db_cursor = db_connection.cursor()
 
 @client.event
 async def on_ready():
@@ -32,6 +37,6 @@ async def on_message(message):
 
 @client.event
 async def on_member_join(member):
-    await registration.memberJoin(member, client)
+    await registration.memberJoin(member, client, db_connection, db_cursor)
 
 client.run(os.environ["DISCORD_BOT_TOKEN"])
--- registration.py
+++ registration.py
@@ -1,11 +1,14 @@
 import json
 import discord
 
-async def memberJoin(member, client):
-    with open("users.json", "r") as fp:
-        userDB = json.load(fp)
+async def memberJoin(member, client, dbConnection, dbCursor):
+    params = (member.id, member.guild.id)
 
-    if not str(member.id) in userDB["users"].keys():
+    dbCursor.execute("SELECT Personal_Channel FROM USERS WHERE User_ID = ? AND Guild = ?", params)
+
+    foundUser = dbCursor.fetchall()
+
+    if len(foundUser) == 0:
         overwrites = {
             member.guild.default_role: discord.PermissionOverwrite(view_channel=False),
             member: discord.PermissionOverwrite(view_channel=True)
@@ -13,19 +16,20 @@ async def memberJoin(member, client):
 
         channel = await member.guild.create_text_channel(f"{member.display_name}-befake", overwrites=overwrites)
 
-        userDB["users"][member.id] = {
-            "username": member.display_name,
-            "real_name": "",
-            "guild": member.guild.id,
-            "personal_channel": channel.id,
-            "groups": []
-        }
+        params = (member.id,
+                  member.guild.id,
+                  channel.id,
+                  member.name,
+                  member.display_name)
 
-        with open("users.json", "w") as fp:
-            json.dump(userDB, fp)
-    else:
-        print(userDB["users"][str(member.id)]["personal_channel"])
+        dbCursor.execute("INSERT INTO USERS (User_ID, Guild, Personal_Channel, Username, Display_Name) VALUES (?, ?, ?, ?, ?)", params)
 
-        channel = client.get_channel(userDB["users"][str(member.id)]["personal_channel"])
+        dbConnection.commit()
+
+    elif len(foundUser) == 1:
+        channel = client.get_channel(foundUser[0][0])
 
         await channel.set_permissions(member, view_channel=True)
+    
+    else:
+        print("ERROR: Duplicate users matching criteria found in database, an unknown problem has occurred.")
